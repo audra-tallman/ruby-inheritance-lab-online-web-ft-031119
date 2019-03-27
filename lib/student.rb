ccfe366a8edd78f5f9905d111f18775125562190
Audra Tallman <audi.tallman@gmail.com>
2019-03-27 20:10:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,16 @@ require_relative "./user.rb"
 
 class Student < User
 
+  def initialize(knowledge)
+    @knowledge= []
+  end 
+  
+  def learn(fact)
+    @knowledge << fact
+  end 
+  
+  def self.knowledge
+    @knowledge
+  end 
+  
 end
